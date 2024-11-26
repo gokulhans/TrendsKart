25ba97d22c1f2dd0b42202a8e39df2ae92bce635
govindhansv <govindhansv333@gmail.com>
2024-11-27 01:21:13 +0530

    new

--- server/app.js
+++ server/app.js
@@ -45,8 +45,9 @@ app.use(logger("dev"));
 
 const corsOptions = {
   // origin: ["https://trends-kart.vercel.app","https://trends-kart-production.vercel.app", "https://trends-kart.vercel.app"],
+  origin: ["https://www.trendkartonline.com", "https://trendkartonline.com"],
   // origin: process.env.CLIENT_URL,
-  origin: "http://localhost:5173",
+  // origin: "http://localhost:5173",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
